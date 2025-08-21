74a47d2bf322f2592420b434e0b97629a058b4a7
este215 <esvargasf@unal.edu.co>
2025-08-21 10:45:41 -0500

    Add files via upload
    Signed-off-by: este215 <esvargasf@unal.edu.co>

---
+++ nosotros/JavaScript.js
@@ -0,0 +1,17 @@
+function flipCard() {
+    const card = document.getElementById('myCard');
+    card.classList.toggle('flipped');
+}
+
+function flipCard2() {
+    const card = document.getElementById('myCard2');
+    card.classList.toggle('flipped');
+}
+function flipCard3() {
+    const card = document.getElementById('myCard3');
+    card.classList.toggle('flipped');
+}
+function flipCard4() {
+    const card = document.getElementById('myCard4');
+    card.classList.toggle('flipped');
+}
